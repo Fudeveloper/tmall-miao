1941c4246735cabd8e0a504b19aab0b481f027a5
lik <1178981326@qq.com>
2022-05-31 21:06:14 +0800

    版本

--- version.js
+++
@@ -1,8 +0,0 @@
-var version = {}
-version.VERSION = '2022618-13'
-version.showVersion = function () {
-    console.log('当前版本：'+version.VERSION)
-    toast('当前版本：'+version.VERSION)
-}
-
-module.exports = version
